2604859e8c26b404bf002515e312d5ee186f6b8e
mgl <1572856541@qq.com>
2023-04-01 16:29:48 +0800

    commit two

--- src/main/java/com/mgl/GitTest.java
+++ src/main/java/com/mgl/GitTest.java
@@ -3,6 +3,7 @@ package com.mgl;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git 01");
+        System.out.println("hello git 02");
 
     }
 }
